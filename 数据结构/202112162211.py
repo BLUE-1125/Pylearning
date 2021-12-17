2205fff15a59fcd422c22abe2e7600eadf4dce30
BLUE-1125 <95302538+BLUE-1125@users.noreply.github.com>
2021-12-17 14:36:15 +0800

    Create 202112162211.py

---
+++ 数据结构/202112162211.py
@@ -0,0 +1,67 @@
+#list
+#a=[66.25,333,333,1,1234.5]
+# print(a.count(333),a.count(66.25),a.count('x'))
+#
+# a.insert(2,-1)
+# print(a)
+# a.append(333)
+# print(a)
+# b=a.index(333)
+# print(b)
+# a.remove(333)
+# print(a)
+# a.reverse()
+# print(a)
+# a.sort()
+# print(a)
+# a.pop(1)
+# print(a)
+
+#tuple
+# t=(12345,54321,'hello')
+# print(t[0])
+# print(t)
+# u=t,(1,2,3,4,5)
+# print(u)
+
+#集合
+# basket={'apple','orange','apple','pear','orange','banana'}
+# print(basket)
+# if 'orange' in basket:
+#     print('true')
+# if 'asdasds' in basket:
+#     print('true')
+# else:
+#     print('false')
+#
+# a=set('abracadabar')
+# b=set('alacazam')
+# print(a)
+# print(b)
+# print(a-b)
+# print(a|b)
+# print(a&b)
+# print(a^b)
+
+#字典
+tel={'jack':4098,'sape':4139}
+tel['guido']=4127
+print(tel)
+print(tel['jack'])
+del tel ['jack']
+print(tel)
+tel['irv']=4127
+print(tel)
+print(tel.keys())
+print(list(tel.keys()))
+print(sorted(tel.keys()))
+
+print("asd" in tel)
+print('guido' in tel)
+
+ddd = dict([('space',4139),('guido',4127),('jack',4098)])
+print(ddd)
+
+print({x:x**2 for x in (2,4,6)})
+
+print(dict(sape=4139,guido=4127,jack=4098))
